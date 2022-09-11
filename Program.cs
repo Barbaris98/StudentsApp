2f756ca2075e0d3c40440ef0ef906d9337290e61
Dmitry Grigorev <d-d12@mail.ru>
2022-09-11 21:00:01 +0400

    1

--- Program.cs
+++ Program.cs
@@ -24,15 +24,46 @@ namespace StudentsApp
 {
     class Program
     {
-        //элемент безопасности, строка подключения будет в этом листе храниться
-        private string connectionString = ConfigurationManager.ConnectionStrings["StudentsDB"].ConnectionString;
+        //1 элемент безопасности, строка подключения будет в этом листе храниться
+        //сделаем их static чтоб работать со static методом Main
+        private static string connectionString = ConfigurationManager.ConnectionStrings["StudentsDB"].ConnectionString;
 
+        // 2 Посредник SqlConnection, черз него будем работать с sql сервером
+        //сделаем их static чтоб работать со static методом Main
+        private static SqlConnection sqlConnection = null;
 
 
         static void Main(string[] args)
         {
+            //3
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
 
-            
+
+            Console.WriteLine("StudentsApp");
+
+            //4
+            SqlDataReader sqlDataReader = null;
+
+            //5 строка в которой будут храиться вводимые значения
+            // от пользователя ???? что такое string.Empty???
+            string command = string.Empty;
+
+
+            while(true)
+            {
+                Console.Write("> ");
+                command = Console.ReadLine();
+
+                //без учёта регистра|| сравнииваем значение ввода Equals
+                if (command.ToLower().Equals("exit"))
+                {
+
+                }
+
+
+            }
+
 
             Console.ReadKey();
         }
