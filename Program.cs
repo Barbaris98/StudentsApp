45723d43911ee9764c67303ed2e1e00a4b464085
Dmitry Grigorev <d-d12@mail.ru>
2022-09-12 21:31:09 +0400

    3 Готово

--- Program.cs
+++ Program.cs
@@ -51,27 +51,104 @@ namespace StudentsApp
             string command = string.Empty;
 
 
-            while(true)
+            while (true)
             {
-                Console.Write("> ");
-                command = Console.ReadLine();
-
-                //без учёта регистра|| сравнииваем значение ввода Equals
-                if (command.ToLower().Equals("exit"))
+                try
                 {
-                    //закрываем соединение, если оно открыто
-                    if (sqlConnection.State == ConnectionState.Open)
+                    Console.Write("> ");
+                    command = Console.ReadLine();
+
+
+                    //Exit
+                    //без учёта регистра|| сравнииваем значение ввода Equals
+                    if (command.ToLower().Equals("exit"))
                     {
-                        sqlConnection.Close();
+                        //закрываем соединение, если оно открыто
+                        if (sqlConnection.State == ConnectionState.Open)
+                        {
+                            sqlConnection.Close();
+                        }
+                        //закрываемм чтеца
+                        if (sqlDataReader != null)
+                        {
+                            sqlDataReader.Close();
+                        }
+                        break;// выкинет из while
                     }
-                    //закрываемм чтеца
-                    if (sqlDataReader != null)
+
+                    //6 созд SqlCommand и передаём      команду(то что вводим) и sqlConnection
+                    SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+
+                    //разбиваем команду по символам на подсткроки по пробелу,,,,,
+                    //Split возвращает массив, поэтому для того что бы получить первое слово
+                    //т.е нулевой элемент в массиве мы должы обратиться к нулевому элементу [0]
+                    //
+                    switch (command.Split(' ')[0].ToLower())
                     {
-                        sqlDataReader.Close();
+                        case "select":
+
+                            //ExecuteReader() - ??
+                            // sqlDataReader здесь будет возвращать нам двумерный массив
+                            // m на n ячкек
+                            sqlDataReader = sqlCommand.ExecuteReader();
+
+                            //Read() перемещает sqlDataReader к следующей записи/строке
+                            // и возвращает булевое значение когда уже вся таблицв прочитана 
+                            while (sqlDataReader.Read())
+                            {
+
+                                // к Ридеру обращаемся по идексатору , те [ID] и тд
+                                Console.WriteLine($"{sqlDataReader["Id"]} {sqlDataReader["FIO"]}" +
+                                    $"{sqlDataReader["Birthday"]} {sqlDataReader["Universuty"]}" +
+                                    $"{sqlDataReader["Group_number"]} {sqlDataReader["Course"]}" +
+                                    $"{sqlDataReader["Averange_score"]}");
+
+
+
+                                Console.WriteLine(new string('-', 30));
+
+
+                            }
+                            //закроем чтеца и потом его по коду заного создадим
+                            if (sqlDataReader != null)
+                            {
+                                sqlDataReader.Close();
+                            }
+
+
+                            break;
+                        case "insert":
+
+                            // ExecuteNonQuery вернёт нам кол-во изменённый строк
+                            // т.е "1"   если не получилось то "0"
+                            Console.WriteLine($"Добавлено: {sqlCommand.ExecuteNonQuery()} строк(а)");
+
+                            break;
+                        case "update":
+
+                            Console.WriteLine($"Изменено: {sqlCommand.ExecuteNonQuery()} строк(а)");
+
+                            break;
+                        case "delete":
+
+                            Console.WriteLine($"Удалено: {sqlCommand.ExecuteNonQuery()} строк(а)");
+
+                            break;
+                        default:
+
+                            Console.WriteLine($"Команда  {command}  некорректна!");
+
+                            break;
                     }
-                    break;// выкинет из while
+
                 }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                }
+
             }
+
 
 
             Console.WriteLine("Для продолжения нажмите любую клавишу ... ");
