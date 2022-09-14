0020228a85bb2b0ffb1dd7042cc079139ef1a55c
Dmitry Grigorev <d-d12@mail.ru>
2022-09-14 15:18:36 +0400

    2 case "sortby"

--- Program.cs
+++ Program.cs
@@ -77,16 +77,22 @@ namespace StudentsApp
                     }
 
                     //6 созд SqlCommand и передаём      команду(то что вводим) и sqlConnection
-                    SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+                    SqlCommand sqlCommand = null;
 
                     //разбиваем команду по символам на подсткроки по пробелу,,,,,
                     //Split возвращает массив, поэтому для того что бы получить первое слово
                     //т.е нулевой элемент в массиве мы должы обратиться к нулевому элементу [0]
+                    //НЕТ Split- разбивает строку на подстроки , начиная с нулевого эл-та массива
                     //
-                    //
-                    switch (command.Split(' ')[0].ToLower())
+
+                    string[] commandArray = command.ToLower().Split(' ');
+
+                    switch (commandArray[0])
                     {
                         case "select":
+
+                            sqlCommand = new SqlCommand(command, sqlConnection);
+
 
                             //ExecuteReader() - ??
                             // sqlDataReader здесь будет возвращать нам двумерный массив
@@ -120,6 +126,8 @@ namespace StudentsApp
                             break;
                         case "insert":
 
+                            sqlCommand = new SqlCommand(command, sqlConnection);
+
                             // ExecuteNonQuery вернёт нам кол-во изменённый строк
                             // т.е "1"   если не получилось то "0"
                             Console.WriteLine($"Добавлено: {sqlCommand.ExecuteNonQuery()} строк(а)");
@@ -127,12 +135,48 @@ namespace StudentsApp
                             break;
                         case "update":
 
+                            sqlCommand = new SqlCommand(command, sqlConnection);
+
                             Console.WriteLine($"Изменено: {sqlCommand.ExecuteNonQuery()} строк(а)");
 
                             break;
                         case "delete":
 
+                            sqlCommand = new SqlCommand(command, sqlConnection);
+
                             Console.WriteLine($"Удалено: {sqlCommand.ExecuteNonQuery()} строк(а)");
+
+                            break;
+                        case "sortby":
+
+                            //sortby fio asc | dec
+                            // сократим команду,            [1] - имя колонки , [2]- параметр asc | dec
+                            sqlCommand = new SqlCommand($"SELECT * FROM [Students] ORDER BY {commandArray[1]} {commandArray[2]}", sqlConnection);
+
+                            sqlDataReader = sqlCommand.ExecuteReader();
+
+                            //Read() перемещает sqlDataReader к следующей записи/строке
+                            // и возвращает булевое значение когда уже вся таблицв прочитана 
+                            while (sqlDataReader.Read())
+                            {
+
+                                // к Ридеру обращаемся по идексатору , те [ID] и тд
+                                Console.WriteLine($"{sqlDataReader["Id"]} {sqlDataReader["FIO"]}" +
+                                    $"{sqlDataReader["Birthday"]} {sqlDataReader["Universuty"]}" +
+                                    $"{sqlDataReader["Group_number"]} {sqlDataReader["Course"]}" +
+                                    $"{sqlDataReader["Averange_score"]}");
+
+
+
+                                Console.WriteLine(new string('-', 30));
+
+
+                            }
+                            //закроем чтеца и потом его по коду заного создадим
+                            if (sqlDataReader != null)
+                            {
+                                sqlDataReader.Close();
+                            }
 
                             break;
                         default:
